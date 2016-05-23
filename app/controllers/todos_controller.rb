aaa5b1e3a393fbc7031a0f612f7bbc6b27c524c7
Sumayyah Perine <sperine01@gmail.com>
2016-05-23 16:31:55 -0400

    finish crud action

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -10,22 +10,30 @@ end
 
 def create
 	@todo = Todo.create(todo_params)
+	redirect_to todo_path(@todo)
 end
 
 def show
 	@todo = Todo.find(params[:id])
+	redirect_to todo_path(@todo)
 end
 
 def edit
-	
+	@todo = Todo.find(params[:id])
 end
 
 def update
-	
+	@todo = Todo.find(params[:id])
+	@todo.update todo_params
+
+	redirect_to todo_path(@todo)
 end
 
 def destroy
-	
+	todo = Todo.find(params[:id])
+    todo.destroy
+
+    redirect_to root_path
 end
 
 private
